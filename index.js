d7496573e8dc15b4b408fa1c4a3093931c7440c8
ayusharoraa <53552723+ayusharoraa@users.noreply.github.com>
2022-08-02 14:05:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('NKCDKSCNKDSCOSD version2 new app is running ')
+  response.send('KKKRNCMKLCS version2 new app is running ')
 }) 
 
 app.listen(app.get('port'), function() {
